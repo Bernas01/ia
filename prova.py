b65d49e7d15484870f03dbd139f76ce7a5d9ed14
Bernas01 <felipebalmeida0108@gmail.com>
2023-11-07 21:56:03 -0300

    Prova

--- prova.py
+++ prova.py
@@ -1,30 +1,20 @@
-NAME	BLOOD STATUS	Species	GENDER	HOUSE	HAIR COLOR	BOGGART	PATRONUS	Nationality	ALIVE?
-Harry Potter	Half-blood	Human	Male	Gryffindor	Jet-black	Dementor	Stag	English	Yes
-Minerva McGonagall	Half-blood	Human	Female	Gryffindor	Black	Lord Voldemort	Cat	Scottish	Yes
-Kingsley Shacklebolt	Pure-blood	Human	Male		Bald		Lynx	British or Irish	Yes
-Aberforth Dumbledore	Half-blood	Human	Male		Grey		Goat	English	Yes
-Remus Lupin	Half-blood	Werewolf	Male	Gryffindor	Light brown flecked with grey	FullÃ¿moon	Wolf	British or Irish	No
-Kreacher		House-elf	Male		White			British	Yes
-Lord Voldemort	Half-blood	Human	Male	Slytherin	Bald	His ownÃ¿corpse	None	English	No
-Bellatrix Lestrange	Pure-blood	Human	Female	Slytherin	Black		None	British	No
-Fenrir Greyback		Werewolf	Male		Grey			British	Unknown
-Nymphadora Tonks	Half-blood	Human	Female	Hufflepuff	Mousy Brown	Losing her abilities	Wolf	British	No
-Lucius Malfoy	Pure-blood	Human	Male	Slytherin	White-blond	Lord Voldemort	None	British	Yes
-Pius Thicknesse	Not Muggle-born	Human	Male		Black and silver		Non-corporeal	British	Unknown
-Scabior	Not Muggle-born	Human	Male	Slytherin				British or Irish	No
-Colin Creeve	Muggle-born	Human	Male	Gryffindor	Mousy	Lord Voldemort		British or Irish	No
-George Weasley	Pure-blood	Human	Male	Gryffindor	Red	Lord Voldemort	Magpie	English	Yes
-Fred Weasley	Pure-blood	Human	Male	Gryffindor	Red	Lord Voldemort	Magpie	English	No
-Lavender Brown	Pure-blood	Human	Female	Gryffindor	Brown	Bloody eyeball	Non-corporeal	British or Irish	No
-Ronald Weasley	Pure-blood	Human	Male	Gryffindor	Red	Aragog	Jack Russell terrier	English	Yes
-Hermione Granger	Muggle-born	Human	Female	Gryffindor	Brown	Failure	Otter	English	Yes
-Neville Longbottom	Pure-blood	Human	Male	Gryffindor	Blond	Severus Snape	Non-corporeal	British	Yes
-Horace Slughorn	Not Muggle-born	Human	Male	Slytherin	Bald		Non-corporeal	British or Irish	Yes
-Filius Flitwick	Half-breed	Human-Goblin	Male	Ravenclaw	Blond	Lord Voldemort	Non-corporeal	British or Irish	Yes
-Vincent Crabbe	Pure-blood	Human	Male	Slytherin	Brown	Lord Voldemort	None	British or Irish	No
-Unknown Male Snatcher	Not Muggle-born	Human	Male		Reddish				No
-Oliver Wood	Not Muggle-born	Human	Male	Gryffindor	Brown	Lord Voldemort		British or Irish	Yes
-Rubeus Hagrid	Half-breed	Human-Giant	Male	Gryffindor	Black	Lord Voldemort	None	English	Yes
-Severus Snape	Half-blood	Human	Male	Slytherin	Black	Lord Voldemort	Doe	English	
-Camelia	Not Muggle-born	Human	Female		Brown				No
-Unknown Giant		Giant	Male						No
+import pandas as pd
+
+# Os dados fornecidos
+data = """NAME    BLOOD STATUS    Species    GENDER    HOUSE    HAIR COLOR    BOGGART    PATRONUS    Nationality    ALIVE?
+Harry Potter    Half-blood    Human    Male    Gryffindor    Jet-black    Dementor    Stag    English    Yes
+Minerva McGonagall    Half-blood    Human    Female    Gryffindor    Black    Lord Voldemort    Cat    Scottish    Yes
+# ... (restante dos dados) ... 
+Unknown Giant    Giant    Male    No"""
+
+# Converter a string de dados em um objeto DataFrame do pandas
+# Aqui estou simulando a leitura dos dados fornecidos, que podem ser obtidos de um arquivo CSV ou de uma fonte de dados
+# Devido ao limite de caracteres, apenas um pequeno trecho foi inserido
+# Certifique-se de que a string contém todos os dados no formato de tabela e não tenha sido truncada
+
+# Supondo que a string é um arquivo CSV ou uma tabela, use o método StringIO para simulá-lo
+from io import StringIO
+df = pd.read_csv(StringIO(data), delimiter='\t', engine='python')
+
+# Exibindo as primeiras linhas para verificar se os dados foram lidos corretamente
+print(df.head())
