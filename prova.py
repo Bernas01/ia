5a148cf9e0231da9eb8dc15801a85d82d80c3d01
Bernas01 <felipebalmeida0108@gmail.com>
2023-11-07 21:57:03 -0300

    PROVA

--- prova.py
+++ prova.py
@@ -1,20 +1,17 @@
+import streamlit as st
 import pandas as pd
+from io import StringIO
 
 # Os dados fornecidos
-data = """NAME    BLOOD STATUS    Species    GENDER    HOUSE    HAIR COLOR    BOGGART    PATRONUS    Nationality    ALIVE?
+data = """
+NAME    BLOOD STATUS    Species    GENDER    HOUSE    HAIR COLOR    BOGGART    PATRONUS    Nationality    ALIVE?
 Harry Potter    Half-blood    Human    Male    Gryffindor    Jet-black    Dementor    Stag    English    Yes
 Minerva McGonagall    Half-blood    Human    Female    Gryffindor    Black    Lord Voldemort    Cat    Scottish    Yes
 # ... (restante dos dados) ... 
 Unknown Giant    Giant    Male    No"""
 
 # Converter a string de dados em um objeto DataFrame do pandas
-# Aqui estou simulando a leitura dos dados fornecidos, que podem ser obtidos de um arquivo CSV ou de uma fonte de dados
-# Devido ao limite de caracteres, apenas um pequeno trecho foi inserido
-# Certifique-se de que a string contém todos os dados no formato de tabela e não tenha sido truncada
-
-# Supondo que a string é um arquivo CSV ou uma tabela, use o método StringIO para simulá-lo
-from io import StringIO
 df = pd.read_csv(StringIO(data), delimiter='\t', engine='python')
 
-# Exibindo as primeiras linhas para verificar se os dados foram lidos corretamente
-print(df.head())
+# Usando Streamlit para mostrar a tabela
+st.write(df)
